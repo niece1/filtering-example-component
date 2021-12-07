697a742a3a37c5fc9df09abbcd11b0085f5840dd
Volodymyr Zhonchuk <vzhonchuk@gmail.com>
2021-12-07 14:00:44 +0200

    :passport_control: Feature: auth added
    Breeze added

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\BookController;
-
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,4 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/books', [BookController::class, 'index']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
